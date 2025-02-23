56838784f3b07477cceaf97cc2ce981fee3dd68c
Linnea Fritz-Watson <watso@MacBook-Pro.local>
2025-02-23 12:07:58 -0700

    very first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+          <h1>Welcome to My React App!</h1>
+          <p>This is a simple React app created with Vite.</p>
+      </div>
+  );
+}
+
+export default App;
+
