d28bd62bea9f3f13cd442df5f9be0815e84b038f
Linnea Fritz-Watson <watso@MacBook-Pro.local>
2025-02-23 19:24:36 -0700

    removed a few more things

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-//for local image placed in src/assets -> import myImage from './assets/myimage.jpg
+
 
 function App() {
   return (
       <div style={{ textAlign: "center", marginTop: "50px" }}>
           <h1>Welcome to Linnea's Fairy Bread page!</h1>
           <p>This is a simple React app created with Vite and hosted on GitHub pages!</p>
-          <ul>
-                <li>First item</li>
-                <li>Second item</li>
-            </ul>
-
       </div>
       
   );
